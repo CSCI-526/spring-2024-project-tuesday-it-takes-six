f1d0b186ba2890bd45b24161309c44fc4b44adb9
Hongen Lei <hongenlei99@gmail.com>
2024-02-26 17:34:55 -0800

    Add End Scene to the game

---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    public void ClickRestartButton ()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void ClickMainMenuButton ()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+  
+}
