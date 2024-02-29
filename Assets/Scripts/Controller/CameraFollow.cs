2a26f30cd93951907ba0b13d06343f740df89465
Keyi Li <23731267+keyi6@users.noreply.github.com>
2024-02-29 12:08:50 -0800

    fixup! Adjust terrain and add map boundary

--- Assets/Scripts/Controller/CameraFollow.cs
+++ Assets/Scripts/Controller/CameraFollow.cs
@@ -7,14 +7,22 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     public GameObject player;
 
+    public GameObject rightBoundary;
+
+    public float rightmostPosition;
+
     void Start()
     {
-
+        rightmostPosition = rightBoundary.transform.position.x - 8;
     }
 
     void LateUpdate()
     {
         // only follow x and z, make y a constant for better view
-        transform.position = new Vector3(player.transform.position.x + 2, 3, player.transform.position.z - 1);
+        transform.position = new Vector3(
+            Mathf.Min(player.transform.position.x + 2, rightmostPosition),
+            3,
+            player.transform.position.z - 1
+        );
     }
 }
