125a02dcb64ba2bd17fa5a2987bf38589decbd5c
OfferPlease <zhouyuxin0106@gmail.com>
2024-03-01 00:45:09 -0800

    Add speedtrigger zoom
    Add a speed trigger zoom so that the corpse can fall onto the correct spot for user to jump on

---
+++ Assets/Scripts/Controller/SpeedTriggerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTriggerController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 Velocity; // speed of the trigger field can be set in inspector
+    
+    private HashSet<GameObject> Objects = new HashSet<GameObject>(); // objects in speed zoom that need to be triggered velocity
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // So far the speed trigger is only applied to enemy corpse
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject enterObject = collider.gameObject;
+        if (collider.CompareTag("Corpse"))
+        {
+            Debug.Log("Speed Trigger: An Enemy Corpse enter a speed trigger zoom");
+            // check if the child object is corpse incase it is body
+            Objects.Add(collider.gameObject);
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        // remove the object from the hashset
+        // assume only coprse are removed
+        if(collider.CompareTag("Corpse"))
+        {
+            Debug.Log("Speed Trigger: An Enemy Corpse leaves a speed trigger zoom");
+            Objects.Remove(collider.gameObject);
+        }
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        
+        foreach (GameObject o in Objects)
+        {
+            o.transform.parent.gameObject.GetComponent<Rigidbody2D>().velocity = Velocity;
+        }
+        
+        
+    }
+}
