3aef3d2f5257072610bb4ef8c5aa6703ca66d0f2
Jinjing Li <lijinjing2000@gmail.com>
2024-03-02 18:10:53 -0800

    try to solve stuck  button

--- Assets/Scripts/Controller/ButtonController.cs
+++ Assets/Scripts/Controller/ButtonController.cs
@@ -13,6 +13,8 @@ public class ButtonController : MonoBehaviour
     private Vector3 Pressedsize = new Vector3(0.8120f, 0.036081f);
     [SerializeField]
     private GameObject nextLevelDoor;
+    private bool isPressed = false;
+    private 
     void Start()
     {
 
@@ -25,9 +27,19 @@ public class ButtonController : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        if(collider.CompareTag("Corpse") && !isPressed)
+        {
+            GameObject Enemy = collider.gameObject.transform.parent.gameObject;
+            Enemy.GetComponent<Rigidbody2D>().AddForce(Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        isPressed = true;
         transform.localScale = Pressedsize;
         if (GlobalData.TimeTenseData.IsPresent())
             nextLevelDoor.SendMessage("setDoorOpen");
@@ -48,7 +60,7 @@ public class ButtonController : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collider)
     {
         // isPressed = false;
-       
+        isPressed = false;
         transform.localScale = size;
         if (GlobalData.TimeTenseData.IsPresent())
             nextLevelDoor.SendMessage("setDoorClosed");
