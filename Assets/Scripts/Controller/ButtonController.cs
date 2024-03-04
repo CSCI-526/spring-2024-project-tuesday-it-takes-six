8443b93798f427c9962865a898192783c7f0e293
OfferPlease <zhouyuxin0106@gmail.com>
2024-03-04 00:10:46 -0800

    Automatically put object on button

--- Assets/Scripts/Controller/ButtonController.cs
+++ Assets/Scripts/Controller/ButtonController.cs
@@ -13,8 +13,8 @@ public class ButtonController : MonoBehaviour
     private Vector3 pressedSize = new Vector3(0.8120f, 0.036081f);
     [SerializeField]
     private GameObject nextLevelDoor;
+
     private bool isPressed = false;
-    private const float forceMag = 120;
     
   
     void Start()
@@ -31,43 +31,39 @@ public class ButtonController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        /*Collider2D collider = collision.collider;
-        if(collider.CompareTag("Corpse"))
-        {
-            if(!isPressed)
-            {
-                Debug.Log("Button: Add force to the corpse");
-                GameObject Enemy = collider.transform.parent.gameObject;
-                Enemy.GetComponent<Rigidbody2D>().AddForce(Vector2.up * forceMag, ForceMode2D.Impulse);
-            }
-        }*/
 
-        isPressed = true;
-        transform.localScale = pressedSize;
-        Debug.Log("Button: Button is pressed");
-        if (GlobalData.TimeTenseData.IsPresent())
-            nextLevelDoor.SendMessage("setDoorOpen");
+        Debug.Log("Button: Corpse enter collision detection area");
+        Collider2D collider = collision.collider;
+        if (collider.CompareTag("Corpse") && !isPressed)
+        {
+            GameObject Enemy = collider.gameObject.transform.parent.gameObject;
+            // Method 1: put the Enemy directly on to the button
+            collider.gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + gameObject.transform.localScale.y * 2);
+
+            isPressed = true;
+            transform.localScale = pressedSize;
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    /*private void OnCollisionExit2D(Collision2D collision)
     {
         isPressed = false;
         transform.localScale = size;
         Debug.Log("Button: Button is unpressed.");
         if (GlobalData.TimeTenseData.IsPresent())
             nextLevelDoor.SendMessage("setDoorClosed");
-    }
+    }*/
 
-    private void OnCollisionStay2D(Collision2D collision)
+    /*private void OnCollisionStay2D(Collision2D collision)
     {
         isPressed = true;
         transform.localScale = pressedSize;
         Debug.Log("Button: Button is pressed");
         if (GlobalData.TimeTenseData.IsPresent())
             nextLevelDoor.SendMessage("setDoorOpen");
-    }
+    }*/
 
-    /*private void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         isPressed = true;
         transform.localScale = pressedSize;
@@ -80,7 +76,7 @@ public class ButtonController : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collider)
     {
-
+        isPressed = true;
         transform.localScale = pressedSize;
         Debug.Log("Button: Button is being pressed");
         if (GlobalData.TimeTenseData.IsPresent())
@@ -96,9 +92,10 @@ public class ButtonController : MonoBehaviour
         Debug.Log("Button: Button is unpressed.");
         if (GlobalData.TimeTenseData.IsPresent())
             nextLevelDoor.SendMessage("setDoorClosed");
+
+       
         
-        
-    }*/
+    }
 
 
     private void FixedUpdate()
